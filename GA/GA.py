a4c55d9381df4b66f6e2feb1309ef18223df04ef
mishIgr <fmikhailv@gmail.com>
2024-07-01 04:21:23 +0300

    frame

--- GA/GA.py
+++ GA/GA.py
@@ -8,11 +8,19 @@ class ParamFitness:
     encore: int
     fine: int
 
+    def __post_init__(self) -> None:
+        if self.encore < 1 or self.fine < 1:
+            raise ValueError('Positive parameters were expected.')
+
 
 @dataclass
 class ParamMutation:
     expansion: float
     narrowing: float
+
+    def __post_init__(self) -> None:
+        if 0 > self.expansion > 1 or 0 > self.expansion > 1:
+            raise ValueError('The parameters cannot represent the probability.')
 
 
 @dataclass
@@ -20,12 +28,20 @@ class ParamProbability:
     crossing: float
     mutation: ParamMutation
 
+    def __post_init__(self) -> None:
+        if 0 > self.crossing > 1:
+            raise ValueError('The parameter(crossing) cannot represent the probability.')
+
 
 @dataclass
 class ParamGeneticAlgorithm:
     probability: ParamProbability
     fitness: ParamFitness
     num_individuals: int
+
+    def __post_init__(self) -> None:
+        if self.num_individuals < 1:
+            raise ValueError('Positive parameter(num_individuals) were expected.')
 
 
 def first_generation(points: list[Point], num_individuals: int) -> list[Rectangle]:

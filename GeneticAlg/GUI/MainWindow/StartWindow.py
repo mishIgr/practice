13522b81770db72bcb68a1319ea86400808e9b10
Egor213 <zamok7707@gmail.com>
2024-07-02 01:12:20 +0300

    add count individe

--- GeneticAlg/GUI/MainWindow/StartWindow.py
+++ GeneticAlg/GUI/MainWindow/StartWindow.py
@@ -10,7 +10,7 @@ from MainWindow import *
 class StartWindow(ctk.CTk):
     def __init__(self) -> None:
         super().__init__(fg_color="#dbdbdb")
-        self.geometry('640x850')
+        self.geometry('640x950')
         self.title("Установка начальных параметров")
         self.resizable(False, False)
         self._start_params_frame: ctk.CTkFrame = StartParamsFrame(self)
@@ -34,7 +34,6 @@ class StartParamsFrame(ctk.CTkFrame):
     def __init__(self, master: ctk.CTk) -> None:
         super().__init__(master=master, width=320, height=450)
         self.grid(row=0, column=0)
-        self.grid_propagate(False)
         self._setter_start_params: ctk.CTkFrame = SetterStartParams(self)
 
     def get_value_start_param(self) -> dict[str, float]:
@@ -45,12 +44,12 @@ class ViewStartParams(ctk.CTkFrame):
     def __init__(self, master: ctk.CTkFrame) -> None:
         super().__init__(master=master, fg_color='#808080', height=110, width=290)
         self.grid(row=1, column=0, padx=20, pady=20, sticky="nsew")
-        self.grid_propagate(False)
         self._state_view_params: dict[str, ctk.CTkLabel] = dict()
         self.__create_view_start_param_label("Шанс мутации: error", 0)
         self.__create_view_start_param_label("Шанс cкрещивания: error", 1)
         self.__create_view_start_param_label("Шанс увеличения прям. при мутации: error", 2)
         self.__create_view_start_param_label("Максимальное количество эпох: error", 3)
+        self.__create_view_start_param_label("Количество индивидов в эпохе: error", 4)
 
     def __create_view_start_param_label(self, text, index_row) -> None:
         view_param_label = ctk.CTkLabel(master=self, width=100, text=text)
@@ -72,6 +71,7 @@ class SetterStartParams(ctk.CTkFrame):
         self.__create_set_start_param_label('Шанс cкрещивания', 2)
         self.__create_set_start_param_label('Шанс увеличения прям. при мутации', 4)
         self.__create_set_start_param_label('Максимальное количество эпох', 6)
+        self.__create_set_start_param_label('Количество индивидов в эпохе', 8)
 
     def __create_set_start_param_label(self, name_param: str, index_row: int) -> None:
         self._string_value_params[name_param] = ctk.StringVar()
